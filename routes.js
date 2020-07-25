2737ce5eea4e86a1a4ec8d9a9aed9211e842ac7d
lucasfeitosa.tech <lucasfeitosa.tech@gmail.com>
2020-07-25 01:19:15 -0300

    [ADD] - Adicionando rotas do projeto easytracking

--- routes.js
+++ routes.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const routes = new express.Router()
 const indexController = require('./website/IndexController');
+const easytrackingController = require('./easytracking/EasyTrackingController');
 
 module.exports = routes;
+
+//Rotas para website
 routes.get('/', indexController.index);
+
+//Routes para EasyTrackingController
+routes.get('/easytracking',easytrackingController.easytrackingRender);  
+routes.post('/easytracking',easytrackingController.easytrackingPost);
+
+routes
