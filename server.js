6591c26234700e052ae0380f7e611d264b04b456
lucasfeitosa.tech <lucasfeitosa.tech@gmail.com>
2020-07-24 23:14:11 -0300

    [ADD] - Adicionando arquivos e configurando servidor

---
+++ server.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+//const qr = require('qr-image');
+// const ejs = require('ejs');
+const app = express();
+
+// app.set('view engine', 'ejs');
+// Set static folder
+// app.use(express.static('static'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(require('./routes'));
+
+
+
+const port = 3000;
+const server = app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
+
+app.get('/teste', function (req, res) {
+  res.send("Hello World");
+});
+
+const path = require('path');
+
