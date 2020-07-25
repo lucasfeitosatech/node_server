558dc0be542e105042a543a91027d3af73e5caab
lucasfeitosa.tech <lucasfeitosa.tech@gmail.com>
2020-07-25 01:33:39 -0300

    [ADD] - Adicionando rotas para filetracking

--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const routes = new express.Router()
 const indexController = require('./website/IndexController');
 const easytrackingController = require('./easytracking/EasyTrackingController');
+const filetrackingController = require('./filetracking/FileTrackingController');
+
 
 module.exports = routes;
 
@@ -11,5 +13,9 @@ routes.get('/', indexController.index);
 //Routes para EasyTrackingController
 routes.get('/easytracking',easytrackingController.easytrackingRender);  
 routes.post('/easytracking',easytrackingController.easytrackingPost);
+
+//Routes para FileTrackingController
+routes.get('/filetracking',filetrackingController.filetrackingRender);  
+routes.post('/filetracking',filetrackingController.filetrackingPost);
 
 routes
